d1f65bb982d30ca3810ae8c2caf9280987f1d6b9
Joseph Lugand <josephlugand@proton.me>
2023-11-20 17:54:09 +0100

    new_service_v1

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -6,9 +6,14 @@ class ServicesController < ApplicationController
   end
 
   def new
+    @service = Service.new
   end
 
   def create
+    @service = Service.new(service_params)
+    @service.save
+  # No need for app/views/restaurants/create.html.erb
+    redirect_to service_path(@service)
   end
 
   def edit
@@ -19,4 +24,11 @@ class ServicesController < ApplicationController
 
   def destroy
   end
+
+  private
+
+  def service_params
+    params.require(:service).permit(:title, :description, :price, :availabilities, :user, :address)
+  end
+
 end
