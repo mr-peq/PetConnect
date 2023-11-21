693b2261a0490ef0893a5e7c13054529c0d55012
Joseph Lugand <josephlugand@proton.me>
2023-11-21 17:52:05 +0100

    user-profil-v1

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   has_many :bookings, through: :services, dependent: :destroy
   has_many :reviews, through: :bookings, dependent: :destroy
   has_one :pet, dependent: :destroy
-
+  has_one_attached :profile_picture
   has_many_attached :photos
 
   def average_rating
