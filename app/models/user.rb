4a8963b89114f53dcbb68812d2b068a251b6298a
Ismael Sentissi <ismael.sentissi@gmail.com>
2023-11-21 10:52:04 +0100

    added the show page, css, and bookings creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,16 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :services, dependent: :destroy
-  has_many :reviews, dependent: :destroy
-  has_many :bookings, dependent: :destroy
-  has_many :bookings, through: :services, dependent: :destroy
-  has_many :reviews, through: :bookings, dependent: :destroy
+  # This will give you all bookings for all services offered by the user
+  has_many :bookings, through: :services
+  # This will give you all reviews for all bookings for all services by the user
+  has_many :reviews, through: :bookings
 
   has_one_attached :photo
+
+  def average_rating
+    total_ratings = self.reviews.sum(:rating)
+    total_reviews = self.reviews.count
+    total_reviews.positive? ? total_ratings.to_f / total_reviews : 0
+  end
 end
