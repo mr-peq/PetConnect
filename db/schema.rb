2b871705caae3b7bf1904827523f2f36438707ab
Theodore Saulnier <theodore1712@yahoo.fr>
2023-11-22 14:46:28 +0100

    Added map without markers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_21_133913) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_22_120141) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -108,6 +108,8 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_21_133913) do
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
     t.string "address"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_services_on_user_id"
   end
 
